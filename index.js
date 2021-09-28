cec7c609448f4876cac4d5902140f0002e501bb4
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2021-09-28 11:54:03 -0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const x = 1 + 1;
 
 console.log(x);
+
+//Node encapsula tudo com uma função anonima que será o wrapper
+//(function(exports, require, module, __filename, __dirname){}
+
+module.exports = function () {
+  console.log("ok");
+};
+
+console.log(__filename);
+console.log(__dirname);
