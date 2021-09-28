8feb5d6d47cafe73f9e35cab6e688040e8884fbf
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2021-09-28 12:07:45 -0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const x = 1 + 1;
-
-console.log(x);
+//npm init -y cria projeto sem questions
 
 //Node encapsula tudo com uma função anonima que será o wrapper
 //(function(exports, require, module, __filename, __dirname){}
@@ -11,3 +9,18 @@ module.exports = function () {
 
 console.log(__filename);
 console.log(__dirname);
+
+//Usando o filesystem
+const fs = require("fs");
+
+fs.mkdir("./teste", () => {
+  console.log("diretorio criado");
+});
+
+fs.writeFile("./teste/teste.txt", "Nadine te amo <3", () => {
+  console.log("ok");
+});
+
+fs.readFile("./teste/teste.txt", (err, data) => {
+  console.log(data);
+});
