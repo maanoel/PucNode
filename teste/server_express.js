4e0a919f52b338cd0c1d56c1d9fafdadacbab450
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2021-09-28 22:34:26 -0300

    testes com middleware

--- teste/server_express.js
+++ teste/server_express.js
@@ -3,7 +3,17 @@ module.exports = () => {
   const app = express();
   const port = 3000;
 
+  const logMiddleware = (req, res, next) => {
+    console.log("API recebeu uma isnformação");
+    next(); //isso continua a execução da pipeline
+  };
+
+  app.use(logMiddleware); //adicionando o midware a pipeline
+  app.use(express.static("./teste/site"));
   app.get("/", (req, res) => {
+    // req.params
+    // req.body
+    // etc...
     res.send("Hello World!");
   });
 
@@ -11,9 +21,9 @@ module.exports = () => {
     res.send("Olá teste!");
   });
 
-  app.post("/post", (req, res) => {
-    req.send({ status: "OK" });
-  });
+  // app.post("/post", (req, res) => {
+  //   req.send({ status: "OK" });
+  // });
 
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
