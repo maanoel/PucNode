3e01d701cbfec5d34571302cffb2df1eb8d25448
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2021-10-11 21:38:45 -0300

    Separação de pastas

--- teste/index.js
+++ teste/index.js
@@ -1,43 +1,13 @@
-function subtracao(numero1, numero2) {
-  let resultado = 0;
-  resultado = numero1 - numero2;
-  console.log(resultado);
-}
+console.log("hello world");
 
-subtracao(15, 48);
+const express = require("express");
+const app = express();
+const port = 3000;
 
-function soma(numero1, numero2) {
-  let resultado = 0;
-  resultado = numero1 + numero2;
-  console.log(resultado);
-}
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-soma(3, 5);
-
-function multiplicacao(numero1, numero2) {
-  let resultado = 0;
-  resultado = numero1 * numero2;
-  console.log(resultado);
-}
-
-multiplicacao(10, 8);
-
-function divisao(numero1, numero2) {
-  let resultado = 0;
-  if (numero1 > 0 && numero2 > 0) {
-    resultado = numero1 / numero2;
-    console.log(resultado);
-  }
-}
-
-divisao(1, 2);
-
-function tabuada(multiplicador) {
-  const meuArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  for (resultado of meuArray) {
-    console.log(resultado * multiplicador);
-  }
-}
-
-tabuada(9);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
